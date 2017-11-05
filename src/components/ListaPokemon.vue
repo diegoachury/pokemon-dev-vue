2386acc3cdb3a00b60cb8adc9b98d149cfc4bccb
AMD-black <dachury@lubu.com.co>
2017-11-05 15:57:35 -0500

    primer commit y creando la primer ruta y el primer componente vue

---
+++ src/components/ListaPokemon.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="content-table">
+    <h2>hola! tablita </h2>
+    <router-link to="/">Go to Bar</router-link>
+    <table>
+    <thead>
+        <tr>
+        <th width="200">Table Header</th>
+        <th>Table Header</th>
+        <th width="150">Table Header</th>
+        <th width="150">Table Header</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        </tr>
+        <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        </tr>
+        <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        </tr>
+        <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        </tr>
+        <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        </tr>
+    </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ListaPokemon',
+  data () {
+    return {
+      msg: 'Welcome pokemon'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
